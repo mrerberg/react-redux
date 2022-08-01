f024ce86489b994fe40651c290f76aaeec4fff35
Mikhail Cheremuhin-Rerberg <mcheremukhin-rerberg@alfabank.ru>
2022-08-01 17:07:14 +0300

    feat(*): init project

---
+++ src/components/todo-list/index.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from "react-redux";
+import { todosSelector } from "../../store/selectors/todo";
+import { Todo } from "../todo";
+
+import styles from './index.module.css';
+
+export const TodoList = () => {
+  const todos = useSelector(todosSelector);
+
+  return (
+    <ul className={styles.list}>
+      {todos.map((todo) => (
+        <Todo key={todo.id} todo={todo} />
+      ))}
+    </ul>
+  );
+};
